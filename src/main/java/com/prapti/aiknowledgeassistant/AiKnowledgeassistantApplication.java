c3a2421830df07fbcd1b0f73736450c6d8664ccf
Rakshith SR <rakshithsr02@gmail.com>
2026-03-13 11:51:23 +0530

    chore: initialize Spring Boot backend project

---
+++ src/main/java/com/prapti/aiknowledgeassistant/AiKnowledgeassistantApplication.java
@@ -0,0 +1,13 @@
+package com.prapti.aiknowledgeassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiKnowledgeassistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiKnowledgeassistantApplication.class, args);
+	}
+
+}
